fa54de41ae6b66eaaaff9751032b0991c19a4117
Nelson <nbritob@hotmail.com>
2019-11-29 10:48:22 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div>
+    <h1>Studio Ghibli: Howl's Moving API</h1>
+
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import FavouriteFilm from './components/FavouriteFilm.vue'
+import FilmDetail from './components/FilmDetail.vue'
+import FilmList from './components/FilmList.vue'
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
