2d862c6c4d0cbd6285ab77625747cd35de4221a6
Nelson <nbritob@hotmail.com>
2019-11-29 11:00:28 +0000

    populating film list

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,19 @@ import FavouriteFilm from './components/FavouriteFilm.vue'
 import FilmDetail from './components/FilmDetail.vue'
 import FilmList from './components/FilmList.vue'
 export default {
-  name: 'app'
-}
+  name: 'app',
+  data() {
+    return {
+      films: []
+    }
+  },
+    mounted() {
+      fetch('https://ghibliapi.herokuapp.com/films')
+      .then(res => res.json)
+      .then(films => this.films = films)
+    }
+  }
+
 </script>
 
 <style lang="css" scoped>
---
+++ src/components/FilmList.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div id="Film-List">
+    <ul>
+      <List-component v-for="film in films" :film="film"></List-component>
+    </ul>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
