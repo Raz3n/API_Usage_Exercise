96dc95112aa7ee97157668fca39dc17aae3ec43b
Nelson <nbritob@hotmail.com>
2019-12-01 11:17:29 +0000

    css added, people api endpoint added

--- src/components/FavouriteFilm.vue
+++ src/components/FavouriteFilm.vue
@@ -1,5 +1,6 @@
+<!-- favourite film should be "Yet to be seen", need to plan better. -->
 <template lang="html">
-  <div>
+  <div class="topcorner">
     <h3>Films yet to see:</h3>
     <ul>
       <li v-for="film in checked">{{film.title}}</li>
@@ -15,8 +16,18 @@ export default {
 </script>
 
 <style lang="css" scoped>
-ul {
-  font-weight: bold;
+.topcorner {
+  position:absolute;
+   top: 80px;
+   right: 40px;
+   border-style:outset;
+   padding-top: 10px;
+   padding-right: 20px;
+   padding-bottom: 10px;
+   padding-left: 20px;
+   font-weight: bold;
+   
 }
+
 
 </style>
--- src/components/FilmDetail.vue
+++ src/components/FilmDetail.vue
@@ -1,10 +1,14 @@
 <template lang="html">
   <div id="film-detail" v-if="film">
+    <div id="detailWrapper">
     <h1>{{film.title}}</h1>
+    <div id="flexWrapper">
     <p><b>Released: </b>{{film.release_date}}</p>
     <p><b>Director: </b> {{film.director}}</p>
     <p><b>Description: </b>{{film.description}}</p>
   </div>
+</div>
+</div>
 
 </template>
 
@@ -16,4 +20,14 @@ export default {
 </script>
 
 <style lang="css" scoped>
+#flexWrapper {
+    box-sizing: border-box;
+    height: 350px;
+    width: 60%;
+    background: #eee;
+    color: #222;
+    padding: 10px;
+    border: 1px solid #ccc;
+  }
+
 </style>
